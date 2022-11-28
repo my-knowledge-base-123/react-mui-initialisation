3a89c64160250af1a57c7d90b49044af0e9534b6
Gary Zhang <gary.zhang@lifebyte.io>
2022-11-28 16:40:00 +1100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,8 @@
 import React from 'react'
 import './App.css'
-import { useAppSelector } from '@/hooks/useAppSelector'
-import { useAppDispatch } from '@/hooks/useAppDispatch'
-import { login } from '@/redux/auth/auth.slice'
 
 function App(): JSX.Element {
-  const auth = useAppSelector((state) => state.auth)
-  const dispatch = useAppDispatch()
-
-  const render = (): JSX.Element => {
-    if (auth.loading === 'pending') {
-      return <div>Fetching</div>
-    }
-
-    if (auth.user != null) {
-      return (
-        <div>
-          <p>
-            Name: {auth.user.name.first} {auth.user.name.last}
-          </p>
-          <p>Email: {auth.user.email}</p>
-        </div>
-      )
-    }
-
-    return <div>No User</div>
-  }
-  return (
-    <div className="App">
-      <div className="card">
-        <button
-          onClick={() => {
-            void dispatch(login())
-          }}
-        >
-          Fetch user
-        </button>
-        {render()}
-      </div>
-    </div>
-  )
+  return <></>
 }
 
 export default App
